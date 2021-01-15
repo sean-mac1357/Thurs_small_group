d076ec592ab170d2abb7b3fe1fab1c74dbaa2729
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-01-14 19:18:46 -0500

    added in drop-down functions with names

--- scripts/scripts2.js
+++ scripts/scripts2.js
@@ -1,11 +1,23 @@
 'use strict';
 
+function getPerson(person) {
+    const url = `https://swapi.dev/api/?people=${person}`;
+    get(url).then(function (response) {
+        updateBody(response.value);
+    })
+}
+
 function getPeople() {
     const url = `https://swapi.dev/api/people/`
     get(url).then(function (response) {
         console.log(response.results);
         createPersonList(response.results)
     })
+}
+
+function updateBody(person) {
+    const paragraph = document.querySelector('div #modal-body p');
+    paragraph.innerHtml = person;
 }
 
 function createPersonList(personList) {
@@ -15,9 +27,16 @@ function createPersonList(personList) {
     personList.map(function(person) {
         const personOption = document.createElement("option")
         personOption.value = person.name;
-        personOption.txt = person.name;
+        console.log(personOption.value);
+        personOption.innerHTML = person.name;
         personSelect.appendChild(personOption);
     });
+    form.appendChild(personSelect);
+
+    personSelect.addEventListener('change', function (event) {
+        getPerson(event.target.value);
+    })
 }
 
 getPeople()
+getPerson('name')
